4aa4550d064b67d45477387cca169ee85c576ab5
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-01-30 14:42:45 -0800

    lab1

---
+++ lab1/about.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+        puts("Hello World!");
+        puts("------------");
+        puts("Author: Patrick San Juan");
+        puts("USF Username: ppsanjuan");
+        puts("Prefered Name: Patrick");
+        puts("Pronouns: he/him/his");
+        return 0;
+}
---
+++ lab1/readme.md
@@ -0,0 +1,3 @@
+# Lab Notes
+
+In the makefile, 755 represents permissions. Each digit represents a specific action on the file: read, write and execute.
