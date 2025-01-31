7c40bde24796fe08cc29eb554169e467dec088ed
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-01-31 11:52:49 -0800

    added about me

--- lab1/about.c
+++ lab1/about.c
@@ -8,5 +8,7 @@ int main(void)
         puts("USF Username: ppsanjuan");
         puts("Prefered Name: Patrick");
         puts("Pronouns: he/him/his");
+        puts("About me:");
+        printf("I am from the Bay Area, raised in a small city in the East Bay: Pinole.\n\nI enjoy tech, music, reading, and going on hikes.\n");
         return 0;
 }
