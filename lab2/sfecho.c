fff6e841b3099fc032cb0c98afa2db277d646127
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-02-08 10:09:36 -0800

    lab2

--- lab2/sfecho.c
+++ lab2/sfecho.c
@@ -1,15 +1,51 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
+int rotate(int *ch, unsigned int places) {
+  int rawChange = *ch + places;
+  if (*ch > 126 || *ch < 33) {
+    return -1;
+  }
+
+  if (rawChange > 126 && *ch <= 126 && *ch > 32) {
+    *ch = rawChange - 126 - 126*(places / 126) + 32;
+  } else {
+    *ch = rawChange;
+  }
+
+  printf("%c", (char)*ch);
+  return 0;
+} 
+
 int main(int argc, char *argv[]) {
   bool newline = true;
+  bool cypher = false;
+  int places;
 
   for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == '-' && argv[i][1] == 'n') {
-      newline = false;
+    if (argv[i][0] == '-') {
+      if (argv[i][1] == 'n') {
+        newline = false;
+      } else if (argv[i][1] == 'r') {
+        cypher = true;
+        places = atoi(argv[i + 1]);
+        i++;
+      } 
     } else {
-      printf("%s ", argv[i]);
+      if (cypher) {
+        size_t size = strlen(argv[i]);
+  
+        for (int j = 0; j < size; j++) {
+          int c = *(argv[i] + j);
+          int* ptr = &c;
+          rotate(ptr, places);
+        }
+        printf(" ");
+      } else {
+        printf("%s ", argv[i]);
+      }
     }
   }
 
