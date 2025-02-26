26ff5c23fc60c1400af31eb1dd888008f8251698
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-02-25 18:10:13 -0800

    draft of lab4

--- lab4/lookup.c
+++ lab4/lookup.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdbool.h>
+
+struct keyword {
+  char *keyword;
+  char *dlim;
+  int keywordIdx;
+  int dlimIdx;
+  int keywordLength;
+  int dlimLength;
+  bool foundKeyword;
+  bool foundDlim;
+  char *captured;
+  bool startCapture;
+  int idx;
+};
 
 int main(int argc, char *argv[]) {
-
-  char *keyword = argv[1];
-  int keywordIdx = 0;
-  int keywordLength = strlen(keyword);
+  struct keyword key;
+  key.keyword = argv[1];
+  key.dlim = argv[2];
+  key.keywordIdx = 0; 
+  key.dlimIdx = 0; 
+  key.keywordLength = strlen(argv[1]);
+  key.dlimLength = strlen(argv[2]);
+  key.foundKeyword = false;
+  key.foundDlim = false;
+  key.idx = 0;
+  key.captured = (char *)malloc(1025);
+  key.startCapture = false;
   
-  int fd = open(argv[2], O_RDONLY);
+  int fd = open(argv[3], O_RDONLY);
   if (fd == -1) {
     perror("open");
     return 1;
@@ -18,21 +42,48 @@ int main(int argc, char *argv[]) {
   char c;
   ssize_t read_sz;
   while((read_sz = read(fd, &c, 1)) > 0) {
-    if (*(keyword + keywordIdx) == c) {
-      keywordIdx++;
-      if (keywordIdx == keywordLength - 1) {
-        keywordIdx = 0;
-        printf("keyword");
+    if (*(key.keyword + key.keywordIdx) == c && key.foundKeyword == false) {
+      key.keywordIdx++;
+      if (key.keywordIdx == key.keywordLength) {
+        key.keywordIdx = 0;
+        key.foundKeyword = true;
+      }
+    } else {
+      key.keywordIdx = 0;
+    }
+
+    if (key.foundKeyword == true && key.foundDlim == false) {
+      if(*(key.dlim + key.dlimIdx) == c) {
+        key.dlimIdx++;
+        if(key.dlimIdx == key.dlimLength) {
+          key.dlimIdx = 0;
+          key.foundDlim = true;
+        }
       }
     }
 
-    // printf("%c", c);
+    if (key.foundDlim == true && key.foundKeyword == true && key.startCapture == false) {
+      if (c != ' ' && *(key.dlim + key.dlimLength - 1) != c) {
+        key.startCapture = true;
+      }
+    }
+
+    if (key.foundDlim == true && key.foundKeyword == true && key.startCapture == true) {
+      if (c != ' ') {
+        *(key.captured + key.idx) = c;
+        key.idx++;
+      } else {
+        key.startCapture = false;
+        printf("%s\n", key.captured);
+        return 0;
+      }
+    }
   }
+
   if (read_sz == -1) {
     perror("read");
     return 1;
   }
-
 
   return 0;
 }
