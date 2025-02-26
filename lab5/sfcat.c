5f40d8ece80ef1418ed5363ed5c40b136bfb39aa
Patrick San Juan <ppsanjuan@dons.usfca.edu>
2025-02-25 18:10:31 -0800

    draft of lab5

---
+++ lab5/sfcat.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct globals {
+    bool help;
+    bool numbering;
+    bool meowed;
+    char str[1024];
+    int opt;
+    int lineNo;
+};
+
+int sizeof_globals = 3 * sizeof(bool) + 1024 + 2 * sizeof(int);
+
+void readFromStdin(struct globals *g) {
+  while(fgets(g->str, sizeof(g->str), stdin) != NULL) {
+    if (g->numbering == true) {
+      printf("%5d\t%s", g->lineNo, g->str);
+      g->lineNo++;
+    } else {
+      printf("%s", g->str);
+    }
+  }
+}
+ 
+int main(int argc, char *argv[]) {
+  struct globals *g = malloc(sizeof_globals);
+  g->help = false;
+  g->numbering = false;
+  g->meowed = false;
+  g->lineNo = 1;
+
+  while((g->opt = getopt(argc, argv, "mnh")) != -1) {
+    switch (g->opt)
+    {
+      case 'n':
+      g->numbering = true;
+      break;
+      case 'm':
+      g->meowed = true;
+      break;
+      case 'h':
+      g->help = true;
+      printf("sfcat\n\nprints the output of the file(s) or stdin.\n\n-n line numbering\n-m add a meow to every line!\n-h help\n");
+      break;
+      
+      default:
+      break;
+    }
+  }
+
+  if(g->help) {
+    return 0;
+  }
+
+  if (optind == argc) {
+    readFromStdin(g);
+  }
+  
+  for (int i = optind; i < argc; i++) {
+    if (strcmp(argv[i], "-") == 0) {
+      readFromStdin(g);
+    } else {
+      FILE* file = fopen(argv[i], "r");
+    
+      if (file == NULL) {
+        perror("fopen");
+        return 1;
+      }
+
+      char buf[1024];
+      char line[1024];
+      char meow[] = " meow!";
+
+      while (fgets(buf, sizeof(buf), file) != NULL) {      
+        strcpy(line, buf);
+        
+        if (g->meowed) {
+          // remove newline from buffered string
+          line[strlen(line)-1] = '\0';
+          strcat(line, meow);
+          strcat(line, "\n");
+        }
+
+        if (g->numbering) {
+          printf("%5d\t%s", g->lineNo, line);
+          g->lineNo++;
+        } else {
+          printf("%s", line);
+        }
+      }
+      fclose(file);
+      printf("\n");
+    }
+  }
+  free(g);
+  return 0;
+}
